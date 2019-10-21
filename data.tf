2f03db7e440843730236c693ca86e5aa242a8d86
Shaung <shaungc@umich.edu>
2019-10-20 22:23:53 -0700

    add support for postgres in k8 cluster, rivernews/appl-tracky-api#13

--- data.tf
+++ data.tf
@@ -4,23 +4,25 @@ variable "secret_pointers" {
   description = "maps name to its full path (formal name), which would be used to organize secrets and aws parameter store."
   type        = list(string)
   default = [
-      "provider/aws/iriversland2-15pro",
-      "provider/digitalocean/shaungc-do",
-      "provider/dockerhub/shaungc",
+    "provider/aws/iriversland2-15pro",
+    "provider/digitalocean/shaungc-do",
+    "provider/dockerhub/shaungc",
 
-      "app/iriversland2",
-      "app/appl-tracky",
+    "app/iriversland2",
+    "app/appl-tracky",
 
-      "database/heroku_iriversland2",
-      "database/heroku_appl-tracky",
-      "database/postgres_cluster_kubernetes",
+    "database/kubernetes_iriversland2",
+    # "database/heroku_iriversland2", # deprecated, using `kubernetes_appl-tracky` instead now
+    "database/kubernetes_appl-tracky",
+    #   "database/heroku_appl-tracky", # deprecated, using `kubernetes_appl-tracky` instead now
+    "database/postgres_cluster_kubernetes",
 
-      "service/gmail",
-      "service/google-social-auth",
+    "service/gmail",
+    "service/google-social-auth",
   ]
 }
 
 data "external" "secret_files" {
-    count = "${length(var.secret_pointers)}"
+  count   = "${length(var.secret_pointers)}"
   program = ["echo", "${file("secrets/${var.secret_pointers[count.index]}.secrets.json")}"]
 }
