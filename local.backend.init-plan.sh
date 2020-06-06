73a259223e4d3a20571627f9ea57cb6a162198fa
Shaung Cheng <shaungc@umich.edu>
2020-06-05 18:07:44 -0700

    update tf command due to image change

--- local.backend.init-plan.sh
+++ local.backend.init-plan.sh
@@ -1,4 +1,4 @@
 docker run --rm -v $(pwd):$(pwd) -w $(pwd) --env ZSH=${ZSH} shaungc/terraform-kubectl-image bash -c '\
-/bin/terraform init -backend-config=local.backend.credentials.tfvars \
-&& /bin/terraform plan \
+terraform init -backend-config=local.backend.credentials.tfvars \
+&& terraform plan \
 '
